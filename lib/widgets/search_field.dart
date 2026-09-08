d6d655a3c7bef827558a9a28c2a156daa03b5d39
pearlkhansa <macbook@pearlkhansas-MacBook-Pro.local>
2026-09-08 08:44:09 +0700

    adding search field

---
+++ lib/widgets/search_field.dart
@@ -0,0 +1,30 @@
+import 'package:flowee_app/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+/**
+ * widegt ini tidak menyimapn teks yang di ketik penggunanya sendiri, setiap kali
+ * pengguna mengetik, onchange akan di panggil dan home screen yang ajab menyimpan teks nya dan lalu memakainya untuk memfilter daftar produk, ini contoh lain
+ * dari pola 'lifting state up'
+ */
+
+class SearchField extends StatelessWidget {
+  const SearchField({super.key, required this.onChanged});
+
+  final ValueChanged<String> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        hintText: 'Cari mawar, tulip, atau anggrek...',
+        hintStyle: TextStyle(color: AppTheme.textSecondary, fontSize: 13.5),
+        prefixIcon: Icon(Icons.search_rounded, color: AppTheme.textSecondary),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
